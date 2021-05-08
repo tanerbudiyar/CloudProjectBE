730aa7cc8a7e860a9e94add5d5ea3f811f4ab78c
Home <Home@Fatoss-MacBook-Air.local>
2021-05-08 06:36:06 -0400

    Back End

---
+++ src/main/java/com/project/employeemanager/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.project.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.employeemanager.exception.UserNotFoundException;
+import com.project.employeemanager.model.Employee;
+import com.project.employeemanager.repo.EmployeeRepo;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
